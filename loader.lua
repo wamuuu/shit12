4a441eeb0e3142e0fca8c71d84ad721b65128c1a
wamuuu <77359658+wamuuu@users.noreply.github.com>
2021-09-16 17:21:04 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2050,4 +2050,4 @@ local maf03={
 }
 if table.find(maf03.roghoul,game.PlaceId)then loadroghoul()end
 if table.find(maf03.bloxfruits,game.PlaceId)then loadbloxfruits()end
-if table.find(maf03.aba,game.PlaceId)then loadbloxfruits()end
+if table.find(maf03.aba,game.PlaceId)then loadaba()end
